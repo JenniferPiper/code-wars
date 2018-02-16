477ac910a884687355607de87d53f693ce9dd58c
Jennifer Williams <jennifer@squareandplum.com>
2018-02-15 19:41:54 -0800

    solve 04-validate-username

--- 04-validate-username/solution.js
+++ 04-validate-username/solution.js
@@ -1,6 +1,10 @@
-// partial solution, passes some tests. Doesn't eliminate strings that are too long or that contain a space.
+// this solution passes all the tests. 
+// ^ start of string
+// ([a-z]|_|\d) lowercase letter OR underscore OR number
+//{4,16} repeated 4 to 16 times
+// $ end of string
 
 function validateUsr(username) {
-  res =  /([a-z]|_|\d){4,16}/.test(username) ;
+  res =  /^([a-z]|_|\d){4,16}$/.test(username) ;
   return res
 }
